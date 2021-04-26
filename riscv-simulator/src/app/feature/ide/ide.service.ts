e796299e1083ed5e97f04c3ff114ef9c4186666a
Julius Plocios <julius.plocios@novare.com.hk>
2021-04-27 00:54:45 +0800

    group program code by section

--- riscv-simulator/src/app/feature/ide/ide.service.ts
+++ riscv-simulator/src/app/feature/ide/ide.service.ts
@@ -17,17 +17,17 @@ export class IdeState {
 export class IdeService extends Store<IdeState> {
 
 
-    constructor() {
-      super(new IdeState());
+  constructor() {
+    super(new IdeState());
   }
 
   public updateInstructions(inst): void {
     this.setState({
-          ...this.state,
-          instructions: inst,
-        });
+      ...this.state,
+      instructions: inst,
+    });
   }
-  
+
   public updateData(data): void {
     this.setState({
       ...this.state,
@@ -35,8 +35,7 @@ export class IdeService extends Store<IdeState> {
     });
   }
 
-  public assembling(data: boolean)
-  {
+  public assembling(data: boolean) {
     console.log('setting isAssembling to ' + data);
     this.setState({
       ...this.state,
@@ -51,15 +50,81 @@ export class IdeService extends Store<IdeState> {
 
   public parseToOpcode() {
     let code = this.state.code;
-    
+
   }
 
   public storeOpcodesToMemory() {
-    
+
   }
 
   public updateCode(newCode) {
     console.log('setting code to: ' + newCode);
+
+    const codeList: string[] = newCode.replace(/(\t|\n)/g, " ").replace(/,/g, "").split(" ").filter(_ => !!_)
+    console.log('parsing', codeList)
+
+    let section = ''
+    let subsection = ''
+    let isMacro = false
+
+    const codeBySection = codeList.reduce((acc, cur) => {
+      if (cur.match(/(.globl|.data|.macro|.text)/g)) {
+        section = cur.slice(1);
+        subsection = ''
+
+        isMacro = cur === '.macro'
+      } else {
+        if (isMacro) {
+          subsection = cur
+          isMacro = false
+        } else if (cur.substr(cur.length - 1) === ':') {
+          subsection = cur.slice(0, -1)
+        } else if (!subsection) {
+          if (acc[section]) {
+            acc[section] = {
+              ...acc[section],
+              default: [
+                ...acc[section]['default'],
+                cur
+              ]
+            }
+          } else {
+            acc[section] = {
+              default: [cur]
+            }
+          }
+        } else {
+          if (acc[section]) {
+            if (acc[section][subsection]) {
+              acc[section] = {
+                ...acc[section],
+                [subsection]: [
+                  ...acc[section][subsection],
+                  cur
+                ]
+              }
+            } else {
+              acc[section] = {
+                ...acc[section],
+                [subsection]: [cur]
+              }
+            }
+          } else {
+            acc[section] = {
+              ...acc[section],
+              [subsection]: [cur]
+            }
+          }
+        }
+      }
+      return acc
+    }, {})
+
+
+    console.log('code by section', codeBySection)
+
+
+
     this.setState({
       ...this.state,
       code: newCode,
